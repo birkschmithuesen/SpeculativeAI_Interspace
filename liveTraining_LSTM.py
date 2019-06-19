a26c82045e16d3f1082cfabaa6aaa89f4af5772e
Marcus Ding <marcusding@me.com>
2019-06-19 21:01:26 +0200

    Fix input shape issue

--- liveTraining_LSTM.py
+++ liveTraining_LSTM.py
@@ -1,11 +1,12 @@
 """
 This program makes predictions for audio input to visualize on the object Interspace.
 The computation process is structured as followed:
-0. trainingsdata + model: loading the trainingsdata from a textfile. Convert the trainingpoints in the right dimension:
-    One line in the text file is one trainingpoint with 30 FFT values and 13824 LED values, separated by tabulators
+0. trainingsdata + model: loading the trainingsdata from a textfile. Convert the
+    trainingpoints in the right dimension: One line in the text file is one trainingpoint
+    with 30 FFT values and 13824 LED values, separated by tabulators
 1. when receiving the the OSC command "/train" the function train_handler executes model.fit
 2. input: receive audio
-3. FFT analyses: seperate continues audio stream into 30 frequency band values with a frequency of 30fps
+3. FFT analysis: separate continues audio stream into 30 frequency bands with a frequency of 30fps
 4. prediction: convert the FFT analyses results to feed into a neural network.
     Predict predict the brightness values of the 13824 Leds of the Interspace object
 """
@@ -32,9 +33,10 @@ import numpy as np
 import os
 from numpy import loadtxt
 
-#the ip and port to send the LED data to. The program Ortlicht receives them via OSC and converts them to ArtNet
+# the ip and port to send the LED data to. The program Ortlicht receives them via OSC and
+# converts them to ArtNet
 UDP_IP = 'localhost'
-UDP_PORT=10005
+UDP_PORT = 10005
 
 model = Sequential()
 
@@ -44,10 +46,13 @@ LSTM_OUT = 512
 
 HIDDEN1_DIM = 1024
 OUTPUT_DIM = 13824
-# input shape is (?, NUM_SOUNDS, INPUT_DIM): ? is the batch size, NUM_SOUNDS is the number of sounds in the
-#                                            sequence and INPUT_DIM is the vector size of each sound
-# output_shape is (?, LSTM_OUT): ? is the batch size and LSTM_OUT is the number of units in the output
-# 'return_sequences' is False because you want only to codify NUM_SOUNDS sounds in one LSTM_OUT-dimensional vector
+# input shape is (?, NUM_SOUNDS, INPUT_DIM): ? is the batch size, NUM_SOUNDS is the number
+#                                            of sounds in the sequence and INPUT_DIM is
+#                                            the vector size of each sound
+# output_shape is (?, LSTM_OUT): ? is the batch size and LSTM_OUT is the number of
+# units in the output
+# 'return_sequences' is False because you want only to codify NUM_SOUNDS sounds in
+# one LSTM_OUT-dimensional vector
 
 
 fft=[]
@@ -59,19 +64,19 @@ def fft_handler(*args):
     """
     this function is called when fft values are received via OSC (from ableton Live)
     """
-    print('received ftt paket. list-length: ',len(fft), '   first arg: ', args[1])
+    print('received ftt paket. list-length: ', len(fft), '   first arg: ', args[1])
     for arg in args[1:]:
-      fft.append(arg)
+        fft.append(arg)
     e1.set()
 
-frameCount=0;
+frameCount = 0
 def frameCountHandler(unused_addr, args):
     """
     a function to synchronize for recording the output of the neural network
     """
     #print('received frameCoount: ', args)
     global frameCount
-    frameCount=args;
+    frameCount = args
 
 def newModel_handler(unused_addr, args):
     """
@@ -79,10 +84,12 @@ def newModel_handler(unused_addr, args):
     ToDo: make it work. Probably the crash is caused because of the multi-threading?
     """
     kerasBackend.clear_session()
-    my_init=keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None)
+    my_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None)
 
-    model.add(Dense(6144, activation='sigmoid', input_dim=30, kernel_initializer=my_init, bias_initializer=my_init))
-    model.add(Dense(13824, activation='sigmoid',kernel_initializer=my_init, bias_initializer=my_init))
+    model.add(Dense(6144, activation='sigmoid', input_dim=30, kernel_initializer=my_init,
+                    bias_initializer=my_init))
+    model.add(Dense(13824, activation='sigmoid',kernel_initializer=my_init,
+                    bias_initializer=my_init))
     sgd = SGD(lr=0.06, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
     model.fit(training_input, training_output, epochs=1, batch_size=32, shuffle=True)
@@ -129,7 +136,7 @@ def ledoutput():
     """
     sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     e2.wait()
-    while true:
+    while True:
         e2.clear()
         prediction_output=dout.pop(0)
         prediction_output=np.multiply(prediction_output,255)
@@ -167,13 +174,11 @@ for counter, row in enumerate(values):
     if counter % NUM_SOUNDS is 0 and counter > 0:
         training_input[batch_counter], training_output[batch_counter] = input_batch.copy(), output_batch.copy()
         batch_counter += 1
-    print(values[counter,:-13824].shape)
-    print(input_batch.shape)
-    input_batch[counter % NUM_SOUNDS] = values[counter,:-13824]
+    input_batch[counter % NUM_SOUNDS] = values[counter,:-OUTPUT_DIM]
     output_batch = values[counter,INPUT_DIM:]
 
 
-print('training_input: ', training_input.shape, 'training_output: ', training_output.shape)
+print('training_input shape: ', training_input.shape, 'training_output shape: ', training_output.shape)
 
 """
 Initialize NeuralNetwork with LSTM.
@@ -213,12 +218,20 @@ t1.start()
 e1.wait()
 while 0<1:
     e1.clear()
-    prediction_input=np.empty([30,1])
-    for x in range(30):
-        prediction_input[x]=fft.pop(0) # get the fft data via OSC from Ableton Live
-    prediction_input.shape=(1,30)
+    prediction_input=np.empty([INPUT_DIM,NUM_SOUNDS])
+    for i in range(NUM_SOUNDS):
+        e1.clear()
+        for x in range(INPUT_DIM):
+            prediction_input[x,i]=fft.pop(0) # get the fft data via OSC from Ableton Live
+        if len(fft)<30:
+            e1.wait()
+    e1.clear()
+    #prediction_input = np.reshape=(prediction_input, (1,NUM_SOUNDS,INPUT_DIM))
+    prediction_input.shape=(1,NUM_SOUNDS,INPUT_DIM)
     prediction_output=model.predict(prediction_input)
     prediction_output=prediction_output.flatten()
+    print("Finished prediction with shape:" + str(prediction_output.shape))
+    print(prediction_output)
     dout.append(prediction_output)
     e2.set()
     if len(fft)<30:
